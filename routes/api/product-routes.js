94978900ce53b152072f417f2676fb63ec6e40ae
J. Michael Brown <jmichaelbrown8@gmail.com>
2022-02-06 21:43:06 -0800

    Added TODO for refactoring the PUT /api/products/:id endpoint, and added functionality to the DELETE /api/products/:id endpoint

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -83,6 +83,9 @@ router.post('/', async (req, res) => {
 });
 
 // update product
+// TODO: this works, but could be refactored with:
+// 1. async/await and 
+// 2. return the fully updated product, rather than the rows updates
 router.put('/:id', (req, res) => {
   // update product data
   Product.update(req.body, {
@@ -124,8 +127,18 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    // delete one product by its `id` value
+    const result = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
