092b114ee961c504b76b684e77a5d0e2279edb08
J. Michael Brown <jmichaelbrown8@gmail.com>
2022-02-06 20:29:21 -0800

    Added POST /api/categories functionality

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -30,8 +30,14 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post('/', async (req, res) => {
+  try {
+    // create a new category
+    const category = await Category.create(req.body);
+    res.status(200).json(category);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
